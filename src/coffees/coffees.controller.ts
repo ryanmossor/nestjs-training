c4aae7056a663c4f89b3f45f9ecea7d4de60e0cd
Ryan Mossor <ryan.mossor@gmail.com>
2022-04-27 09:15:02 -0500

    GET/POST/PATCH/DELETE request modules complete

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-    @Get()
-    findAll() {
-        return 'This action returns all coffees';
-    }
+  @Get()
+  findAll() {
+    return 'This action returns all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
+  }
 }
