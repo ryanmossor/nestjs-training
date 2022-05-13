b5e380aaf6610c6c9e128eca2b84cdb9bc414ea8
Ryan Mossor <ryan.mossor@gmail.com>
2022-05-13 11:27:56 -0500

    Created flavor entity and established relationship with coffee entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
 
 @Entity()
 export class Coffee {
@@ -11,6 +18,9 @@ export class Coffee {
   @Column()
   brand: string;
 
-  @Column('json', { nullable: true })
+  @JoinTable() // specifies the owner side of the relationship between coffee/flavor
+  // type => Flavor specifies the type of relationship; returns a reference to the Flavor entity
+  // flavor => flavor.coffees specifies what coffee is within the Flavor entity
+  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees)
   flavors: string[];
 }
