0d878bc3d83d9c00582d0bee43c20022fcd76458
Ryan Mossor <ryan.mossor@gmail.com>
2022-05-13 15:36:52 -0500

    Completed cascading inserts/updates lesson

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -4,12 +4,15 @@ import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
 @Injectable()
 export class CoffeesService {
   constructor(
     @InjectRepository(Coffee)
     private readonly coffeeRepository: Repository<Coffee>,
+    @InjectRepository(Flavor)
+    private readonly flavorRepository: Repository<Flavor>,
   ) {}
 
   findAll() {
@@ -28,23 +31,37 @@ export class CoffeesService {
     return coffee;
   }
 
-  create(createCoffeeDto: CreateCoffeeDto) {
+  async create(createCoffeeDto: CreateCoffeeDto) {
+    const flavors = await Promise.all(
+      // loop through all flavors in DTO and use preloadFlavorByName() to make sure they're properly populated
+      // await and Promise.all() waits for entire array of promises finishes before executing further code
+      // flavors variable becomes the array of Flavor entities
+      createCoffeeDto.flavors.map((name) => this.preloadFlavorByName(name)),
+    );
+
     // create coffee class instance based on partial DTO and save to variable coffee
-    const coffee = this.coffeeRepository.create(createCoffeeDto);
+    const coffee = this.coffeeRepository.create({
+      // merge createCoffeeDto with updated Flavor entities
+      ...createCoffeeDto,
+      flavors,
+    });
     // save to DB
     return this.coffeeRepository.save(coffee);
   }
 
   async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
-    /* preload() creates new entity based on object passed to it.
-    preload() looks to see if entity already exists in DB; if so, retrieves it and everything related to it.
-    If entity exists, preload() replaces all values with the new ones passed here in updateCoffeeDto */
+    const flavors =
+      updateCoffeeDto.flavors && // checks that a flavors property exists before running map()
+      (await Promise.all(
+        updateCoffeeDto.flavors.map((name) => this.preloadFlavorByName(name)),
+      ));
+
     const coffee = await this.coffeeRepository.preload({
       id: +id,
       ...updateCoffeeDto,
+      flavors,
     });
     if (!coffee) {
-      // preload() returns undefined if id passed in is not found in DB
       throw new NotFoundException(`Coffee #${id} not found`);
     }
     return this.coffeeRepository.save(coffee);
@@ -54,5 +71,13 @@ export class CoffeesService {
     // retrieve coffee based on ID; if exists, remove it
     const coffee = await this.findOne(id);
     return this.coffeeRepository.remove(coffee);
+  }
+
+  private async preloadFlavorByName(name: string): Promise<Flavor> {
+    const existingFlavor = await this.flavorRepository.findOne({ name });
+    if (existingFlavor) {
+      return existingFlavor;
+    }
+    return this.flavorRepository.create({ name });
   }
 }
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -21,6 +21,6 @@ export class Coffee {
   @JoinTable() // specifies the owner side of the relationship between coffee/flavor
   // type => Flavor specifies the type of relationship; returns a reference to the Flavor entity
   // flavor => flavor.coffees specifies what coffee is within the Flavor entity
-  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees)
-  flavors: string[];
+  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees, { cascade: true })
+  flavors: Flavor[];
 }
